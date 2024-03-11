23adadc89922f4b79b5d9be8bfdeae1db438a310
Service Web <sw@gssmail.com>
2024-03-11 08:53:57 -0500

    Commit changes in C:\Websites\Git\emarlow\3919\GCG_3919_FIT_Inv_FAI.sql

---
+++ GCG_3919_FIT_Inv_FAI.sql
@@ -0,0 +1,8 @@
+CREATE TABLE "GCG_3919_FIT_Inv_FAI"(
+ "Part" CHAR(20),
+ "Rev" CHAR(3),
+ "Loc" CHAR(2),
+ "Approv_Pend" CHAR(1),
+ "Approv_Date" DATE,
+ "Date_Submitted" DATE,
+ "Approved_By" CHAR(20));
